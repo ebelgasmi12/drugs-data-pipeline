31c8fe6412395ef5160093c66d83ed5c0b4d5cc3
El Mehdi Belgasmi <elmehdibelgasmi@MacBook-Pro-de-El.local>
2021-03-25 06:03:04 +0100

    Add Terraform deployment.

--- cicd/deploy/kube-tf/apply.sh
+++ cicd/deploy/kube-tf/apply.sh
@@ -2,6 +2,10 @@
 # Get script directory (allows to launch script from anywhere)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+echo "Execucting terraform in folder: $SCRIPT_DIR"
+echo "Files in folder:"
+echo "ls $SCRIPT_DIR"
+
 # Terraform deploy commands
 terraform init $SCRIPT_DIR
 terraform get $SCRIPT_DIR
