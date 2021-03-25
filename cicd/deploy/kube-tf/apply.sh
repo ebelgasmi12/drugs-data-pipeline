ebc5d8ff30069ce5fc138b3f7ad2799b460703ac
El Mehdi Belgasmi <elmehdibelgasmi@MacBook-Pro-de-El.local>
2021-03-25 08:13:10 +0100

    Add GCS as backend for Terraform.

--- cicd/deploy/kube-tf/apply.sh
+++ cicd/deploy/kube-tf/apply.sh
@@ -4,11 +4,21 @@
 # Get script directory (allows to launch script from anywhere)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo "Execucting terraform in folder: $SCRIPT_DIR"
-echo "Files in folder:"
-echo "ls $SCRIPT_DIR"
+# If the Branch is PROD
+if [ $BRANCH_NAME = "prod" ]
+then
+    echo "********************************* APPLYING ************************************"
+    echo "Execucting terraform command in folder: $SCRIPT_DIR"
+    echo "*******************************************************************************"
+fi
+    # Terraform deploy commands
+    terraform init $SCRIPT_DIR
+    terraform get $SCRIPT_DIR
+    terraform apply -auto-approve -var="project=$PROJECT_ID" $SCRIPT_DIR
+else
+    echo "***************************** SKIPPING APPLYING *******************************"
+    echo "Branch '$BRANCH_NAME' does not represent an oficial environment."
+    echo "*******************************************************************************"
+fi
 
-# Terraform deploy commands
-terraform init $SCRIPT_DIR
-terraform get $SCRIPT_DIR
-terraform apply -auto-approve -var="project=$PROJECT_ID" $SCRIPT_DIR
+
